eb971d4d06d45e84fdfb9313c2b2c126240c4339
Leonil Ifrah <133550908+lifrah@users.noreply.github.com>
2024-12-28 22:06:49 +0800

    Update Chapter 1.rkt

--- Chapter 1.rkt
+++ Chapter 1.rkt
@@ -672,9 +672,73 @@
 ;It will throw an error that new-f isn't a
 ;procedure due to argument provided is defined is also procedureless.
 
-(define (new-f g) (g 2))
+;(define (new-f g) (g 2))
 
-(new-f new-f)
+;(new-f new-f)
+
+
+(define (close-enough? x y)
+  (< (abs (- x y)) 0.0001))
+
+(define (search f neg-point pos-point)
+  (let ((midpoint (average neg-point pos-point)))
+    (if (close-enough? neg-point pos-point)
+        midpoint
+        (let ((test-value (f midpoint)))
+          (cond ((positive? test-value)
+                 (search f neg-point midpoint))
+                ((negative? test-value)
+                 (search f midpoint pos-point))
+                (else midpoint))))))
+
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+        (b-value (f b)))
+    (cond ((and (negative? a-value) (positive? b-value))
+           (search f a b))
+          ((and (negative? b-value) (positive? a-value))
+           (search f b a))
+          (else
+           (error "Values are not not of opposite sign" a b)))))
+
+;(half-interval-method sin 2.0 4.0)
+
+;(half-interval-method (lambda (x) (- (cube x) (* 2 x) 3)) 1.0 3.0)
+
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+;(fixed-point cos 1.0)
+
+;(fixed-point (lambda (y) (+ (sin y) (cos y))) 1.0)
+
+;(define (sqrt x)
+;  (fixed-point (lambda (y) (/ x y)) 1.0))
+
+(define (sqrt x)
+  (fixed-point (lambda (y) (average y (/ x y))) 1.0))
+
+;(sqrt 2)
+
+
+;Exercise 1.35
+(define (phi x)
+  (fixed-point (lambda (y) (+ 1.0 (/ 1.0 y))) x))
+
+(phi 2)
+
+;Exercise 1.36
+
 
 
         
